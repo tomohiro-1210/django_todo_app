acebbd6e2d4dacb366b4e401bf6efd0bffebccb9
tomohiro_0225 <express22212270@gmail.com>
2024-07-24 15:19:37 +0900

    リストの表示

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import todoModel
 
 # Create your views here.
+class TodoList(ListView):
+    template_name = 'list.html'
+    model = todoModel
+

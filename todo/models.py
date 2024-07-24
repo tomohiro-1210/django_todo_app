d8c38f20e624f9bf7461cf7bda5e045485f6f9a3
tomohiro_0225 <express22212270@gmail.com>
2024-07-24 15:07:49 +0900

    DBの作成

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
 # Create your models here.
+
+class todoModel(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.title

ab3abf102ca3b67111a9ed04ea943873f924bb70
tomohiro_0225 <express22212270@gmail.com>
2024-07-25 12:07:04 +0900

    Todoアプリのタスク登録機能作成
    views.py
    model->DBの名前
    fields->使うカラム？
    success_url->送信成功した後のリダイレクト先

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import todoModel
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+# List(一覧画面)
 class TodoList(ListView):
     template_name = 'list.html'
     model = todoModel
 
-
+# Detail(詳細画面)
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = todoModel
+
+# Create（登録、作成）
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = todoModel
+    fields = ('title','memo','priority','duedate')
+    success_url = reverse_lazy('list')

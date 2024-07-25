c7529a6b1b6cb3f113375d76454dffbf3e263689
tomohiro_0225 <express22212270@gmail.com>
2024-07-25 11:33:08 +0900

    見た目の修正とDBとClassの連携

--- todo/models.py
+++ todo/models.py
@@ -3,11 +3,17 @@ from django.db import models
 
 # Create your models here.
 
+CHOICE = (('red','high'),('green','normal'),('blue','low',))
+
 class todoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(max_length=2000)
 
-    
+    priority = models.CharField(
+        max_length=50,
+        choices = CHOICE
+    )
+    duedate = models.DateField()
 
     def __str__(self):
         return self.title
